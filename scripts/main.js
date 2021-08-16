5b53b2f5e8656f1a01dc58eed6f5d549f45a9e11
Atharva Kunte <athu.kunte@gmail.com>
2021-08-16 19:25:11 +0530

    change button and form styling
    Signed-off-by: Atharva Kunte <athu.kunte@gmail.com>

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,5 @@ function generateGradient() {
     var col1 = document.getElementById('color1').value;
     var col2 = document.getElementById('color2').value;
     document.getElementById('gradient-box').style.backgroundImage = `linear-gradient(${angle}deg, ${col1}, ${col2})`;
-    document.getElementById('code-text').innerHTML = `<pre>background-image: linear-gradient(${angle}deg, ${col1}, ${col2})</pre>`
+    document.getElementById('code-text').innerHTML = `<pre>background-image: linear-gradient(${angle}deg, ${col1}, ${col2});</pre>`
 }
