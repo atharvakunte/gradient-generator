56ec6234180c1e1f7d1e845bad448256eb60d0de
Atharva Kunte <athu.kunte@gmail.com>
2021-08-16 19:12:44 +0530

    change path of script remove console logs Signed-off-by: Atharva Kunte <athu.kunte@gmail.com>

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,6 @@ function generateGradient() {
     var angle = document.getElementById('angle').value;
     var col1 = document.getElementById('color1').value;
     var col2 = document.getElementById('color2').value;
-
-    console.log(angle);
-    console.log(col1);
-    console.log(col2);
-
     document.getElementById('gradient-box').style.backgroundImage = `linear-gradient(${angle}deg, ${col1}, ${col2})`;
     document.getElementById('code-text').innerHTML = `<pre>background-image: linear-gradient(${angle}deg, ${col1}, ${col2})</pre>`
 }
